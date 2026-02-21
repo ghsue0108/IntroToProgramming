b912e25add73f702025857c4f87fc9069e44c677
ghsue0108 <ghsuee@gmail.com>
2026-02-21 19:33:43 +0400

    Delete BruteForceAttack.py

--- BruteForceAttack.py
+++
@@ -1,9 +0,0 @@
-correct_password = "12345"
-
-user = input("Enter password: ")
-
-while user != correct_password:
-    print("Hint: Count 1-5")
-    user = input("Enter password: ")
-
-print("Access granted!")
